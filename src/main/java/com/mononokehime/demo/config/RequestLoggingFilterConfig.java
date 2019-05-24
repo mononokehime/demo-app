1e4673c30d66ccf6b129cbf486d8d97a0a0ec020
Fergus MacDermot <fergusmacdermot@gmail.com>
2019-05-24 23:58:07 +0800

    Logging request data

---
+++ src/main/java/com/mononokehime/demo/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,44 @@
+package com.mononokehime.demo.config;
+
+/*-
+ * #%L
+ * Spring Boot Demo App
+ * %%
+ * Copyright (C) 2019 Mononokehime
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+    private static final int PAYLOAD_LENGTH = 10000;
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+                = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(PAYLOAD_LENGTH);
+        filter.setIncludeHeaders(true);
+        filter.setAfterMessagePrefix("REQUEST DATA : ");
+        return filter;
+    }
+}
+
